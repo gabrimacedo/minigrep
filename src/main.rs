3141f94f117b737ed7f34d5f9e0671f466a277c2
macedo <gabrimacedo@posteo.com>
2026-01-27 19:08:22 -0300

    finish read from sdtin/dir/file, TODO: introduce lvl flags

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,39 @@ use std::{
     env,
     fs::{self, File},
     io::{self, IsTerminal, Read, stdin},
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 
-fn read_from_file(path: &str, s: &mut String) {
-    let path = Path::new(path);
-    let display = path.display();
+fn read_from_file(path: PathBuf, s: &mut String) -> io::Result<()> {
+    let mut file = File::open(&path).map_err(|e| {
+        io::Error::new(
+            e.kind(),
+            format!("could not open file {}: {}", path.display(), e),
+        )
+    })?;
 
-    // get file handle
-    let mut file = match File::open(path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
+    file.read_to_string(s).map_err(|e| {
+        io::Error::new(
+            e.kind(),
+            format!("could not read file {} to string: {}", path.display(), e),
+        )
+    })?;
 
-    // read file
-    if let Err(why) = file.read_to_string(s) {
-        println!("couldn't open {}: {}", display, why);
-    };
+    Ok(())
 }
 
-fn read_from_dir(s: &mut String) -> io::Result<()> {
-    let dir = env::current_dir()?;
+fn read_from_dir(s: &mut String, path: PathBuf) -> io::Result<()> {
     let mut queue: VecDeque<PathBuf> = VecDeque::new();
-    queue.push_front(dir);
+    queue.push_front(path);
 
     // visit a node
     while let Some(path) = queue.pop_front() {
-        visit_node(&mut queue, path, s)?;
+        match visit_node(&mut queue, path, s) {
+            Err(e) if e.kind() == io::ErrorKind::InvalidData => continue,
+            Err(e) => return Err(e),
+            Ok(_) => {}
+        }
     }
-
     Ok(())
 }
 
@@ -44,8 +48,7 @@ fn visit_node(queue: &mut VecDeque<PathBuf>, current: PathBuf, s: &mut String) -
             queue.push_back(path);
             continue;
         }
-        let path = path.to_str().unwrap();
-        read_from_file(path, s);
+        read_from_file(path, s)?;
     }
     Ok(())
 }
@@ -55,25 +58,42 @@ fn main() {
 
     // get pattern TODO: handle none
     let pattern = args.get(1).unwrap();
-
+    // string to hold the reads
     let mut s = String::new();
 
-    // read from file if specified, stdin otherwise, TODO: curr dir otherwsise
     match args.get(2) {
-        Some(path) => read_from_file(path, &mut s),
+        // if we get arg, check if dir or file
+        Some(path) => {
+            let path = PathBuf::from(path);
+            if path.is_dir() {
+                if let Err(why) = read_from_dir(&mut s, path) {
+                    println!("could not read from directory: {}", why)
+                };
+            } else {
+                let _ = read_from_file(path, &mut s);
+            }
+        }
+        // if no arg, check stdin
         None => {
             if stdin().is_terminal() {
-                let _ = read_from_dir(&mut s);
-            } else {
+                let current = env::current_dir().unwrap();
+                let _ = read_from_dir(&mut s, current);
+            }
+            // else run on current dir
+            else {
                 let sin = stdin().read_to_string(&mut s).unwrap();
                 println!("copied {} bytes", sin);
             }
         }
     }
 
+    find_hits(&s, pattern);
+}
+
+fn find_hits(s: &str, pattern: &str) {
     let red_start = "\x1b[31m";
     let red_end = "\x1b[0m";
-    // search for pattern
+
     for line in s.lines() {
         if line.contains(pattern) {
             let colored = line.replace(pattern, format!("{red_start}{pattern}{red_end}").as_str());

20f87f74d3574732454b090b76f5f1c3114c55ef
macedo <gabrimacedo@posteo.com>
2026-01-30 22:14:05 -0300

    [refactor] print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ use std::{
     process,
 };
 
+// TODO: add command line flag or ENV variable for lvl control and case sensitivity
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let has_stdin = !stdin().is_terminal();
     let config = Config::build(&args, has_stdin).unwrap_or_else(|e| {
-        println!("problem parsing arguments: {e}!");
+        eprintln!("problem parsing arguments: {e}!");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("application error: {e}");
+        eprintln!("application error: {e}");
         process::exit(1);
     };
 }
